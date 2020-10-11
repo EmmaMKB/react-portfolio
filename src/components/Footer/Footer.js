38cdeef1c4588bb9a3cc2f96f1eceb68e9304da9
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-11 00:16:28 -0700

    ✨ Add social links in Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./footer.css";
+import {FaFacebookF, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
+
 
 const Footer = () => {
 
     return(
         <div className="footer">
-
+            <ul className="social">
+                <li className="social-link"><a href="#" target="_blank"><FaFacebookF /></a></li>
+                <li className="social-link"><a href="#" target="_blank"><FaInstagram /></a></li>
+                <li className="social-link"><a href="#" target="_blank"><FaLinkedinIn /></a></li>
+            </ul>
         </div>
     )
 }
