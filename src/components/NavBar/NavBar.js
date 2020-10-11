a3dd0ce25870b78208dfe1fa9c1e4c6a26dc3cfd
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-10 23:19:24 -0700

    ✨ Add navbar items

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./navbar.css";
 
 const NavBar = () => {
     return(
@@ -6,6 +7,19 @@ const NavBar = () => {
             <div className="logo-img">
                 <img src={require("../../assets/images/logo.png")} alt=""/>
             </div>
+            <nav className="navbar-items">
+                <li className="navbar-link">
+                    <a href="#">Home</a>
+                </li>
+                <li className="navbar-link">
+                    <a href="#">Skills</a>
+                </li>
+                <li className="navbar-link">
+                    <a href="#">Portfolio</a>
+                </li> <li className="navbar-link">
+                    <a href="#">Contact</a>
+                </li>
+            </nav>
         </div>
     )
 }
