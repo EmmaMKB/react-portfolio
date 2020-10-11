172393cf509c23381d5edee95044fb1465592586
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-11 00:41:24 -0700

    ✨ Add Skills page in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from "./pages/home/Home";
 import "./assets/stylesheets/style.css"
+import Skills from "./pages/skills/Skills";
 
 function App() {
   return (
     <React.Fragment>
       <Home />
+      <Skills />
     </React.Fragment>
   );
 }
