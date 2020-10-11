f1f661c63af01ad664a6fdff43b4aa115e371321
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-10 23:54:37 -0700

    ✨ Add BannerSection in Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import BannerSection from "../../layout/banner-section/banner-section";
 
 const Home = () => {
 
     return(
         <div className="page">
             <NavBar />
+            <BannerSection />
         </div>
     )
 }
