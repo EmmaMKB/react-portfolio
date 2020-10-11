b675bbc52fe93f4d3f2d5dd72543183312ec2b86
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-10 22:40:01 -0700

    ✨ Add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return(
+        <div className="navbar">
+            <div className="logo-img">
+                <img src={require("../../assets/images/logo.png")} alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
