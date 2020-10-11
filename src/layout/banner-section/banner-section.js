a8db957068e4bddf1d76e10ebc892a6948562a7f
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-10 23:53:22 -0700

    ✨ Implement banner-section layout

--- src/layout/banner-section/banner-section.js
+++ src/layout/banner-section/banner-section.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./banner-section.css";
 
 const BannerSection = () => {
 
     return(
         <div className="banner-section">
-
+            <div className="content">
+                <h1><span className="orange-text">FullStack</span> developer</h1>
+                <h1>Graphic <span className="orange-text">designer</span></h1>
+            </div>
         </div>
     )
 }
