716ca39510b40157cf11f5d79a82bdb8e4e0b54c
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-11 00:52:50 -0700

    ✨ Add Fullpage component in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Home from "./pages/home/Home";
 import "./assets/stylesheets/style.css"
 import Skills from "./pages/skills/Skills";
+import Fullpage, { FullPageSections, FullpageSection } from "@ap.cx/react-fullpage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <React.Fragment>
-      <Home />
-      <Skills />
-    </React.Fragment>
+    <Fullpage>
+        <FullPageSections>
+            <FullpageSection>
+                <Home />
+            </FullpageSection>
+            <FullpageSection>
+                <Skills />
+            </FullpageSection>
+            <Footer />
+        </FullPageSections>
+    </Fullpage>
   );
 }
 
