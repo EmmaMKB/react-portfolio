5794fa3cec748c141400e51f153d16403b0e834a
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-10 22:39:26 -0700

    ✨ Add Home page component

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+
+const Home = () => {
+
+    return(
+        <div className="page">
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home;
