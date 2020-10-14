a063ed97d6f492e46d330d87ac9af226440d1281
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-14 11:09:35 -0700

    ✨ Add Navbar in Skills page

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./skills.css";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Skills = () => {
 
     return(
         <div className="page skills__page">
+            <NavBar dark={true} />
             <h1>Skills</h1>
         </div>
     )
